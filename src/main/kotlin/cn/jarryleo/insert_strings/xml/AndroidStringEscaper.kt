5265930885bc56db09479d43c6b9e220ed24f7a5
jarryleo <yjtx256@qq.com>
2026-06-16 10:49:14 +0800

    [add] CDATA 不经过转义

--- src/main/kotlin/cn/jarryleo/insert_strings/xml/AndroidStringEscaper.kt
+++ src/main/kotlin/cn/jarryleo/insert_strings/xml/AndroidStringEscaper.kt
@@ -4,7 +4,28 @@ object AndroidStringEscaper {
     private val entityPattern = Regex("""&(amp|lt|gt|quot|apos|#[0-9]+|#x[0-9a-fA-F]+);""")
 
     fun escape(text: String): String {
-        return escapeXmlText(escapeAndroidText(text))
+        // CDATA 段（<![CDATA[...]]>）内部的文字保持原样，不经过 Android/XML 转义
+        val builder = StringBuilder(text.length)
+        var index = 0
+        while (index < text.length) {
+            val cdataStart = text.indexOf("<![CDATA[", index)
+            if (cdataStart == -1) {
+                builder.append(escapeXmlText(escapeAndroidText(text.substring(index))))
+                break
+            }
+            if (cdataStart > index) {
+                builder.append(escapeXmlText(escapeAndroidText(text.substring(index, cdataStart))))
+            }
+            val cdataEnd = text.indexOf("]]>", cdataStart + 9)
+            if (cdataEnd == -1) {
+                // 没有闭合的 CDATA，按普通文本处理
+                builder.append(escapeXmlText(escapeAndroidText(text.substring(cdataStart))))
+                break
+            }
+            builder.append(text.substring(cdataStart, cdataEnd + 3))
+            index = cdataEnd + 3
+        }
+        return builder.toString()
     }
 
     private fun escapeAndroidText(text: String): String {
